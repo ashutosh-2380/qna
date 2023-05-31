b676bffe6e87f6a0f147404e54d7bf79128b4878
ashutosh-2380 <126765450+ashutosh-2380@users.noreply.github.com>
2023-05-31 18:23:56 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from transformers import pipeline
+
+# Load the question answering model
+model = pipeline("question-answering")
+
+# Set up the Streamlit app
+st.title("Question Answering App")
+
+# Input fields for the user to enter question and context
+question = st.text_input("Enter your question:")
+context = st.text_area("Enter the context:")
+
+# Generate the answer when the user clicks the "Answer" button
+if st.button("Answer"):
+    # Perform question answering
+    answer = model(question=question, context=context)
+
+    # Display the answer
+    st.write("Answer:", answer["answer"])
+    st.write("Confidence score:", answer["score"])
